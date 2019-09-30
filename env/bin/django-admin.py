7e5fd6173632f9217f3d0297bd1e7ebc3cf3186e
Benjamin Choi <choi.bc97@gmail.com>
2019-09-30 18:40:57 -0500

    Create python virtual environment for django

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/choibc/Documents/Courses/Year 5/CSE 437S/picturesque/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
