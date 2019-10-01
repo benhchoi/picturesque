10eb85e98aba4bb84cff6e0526116cdd1e395a87
Benjamin Choi <choi.bc97@gmail.com>
2019-09-30 23:35:08 -0500

    Clean up stuff with gitignore

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/choibc/Documents/Courses/Year 5/CSE 437S/picturesque/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
