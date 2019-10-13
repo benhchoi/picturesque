d322896ed8f6fe107866dfaad5e937cba391ee4f
Benjamin Choi <choi.bc97@gmail.com>
2019-10-13 20:11:00 +0000

    Finish initial frontend setup

--- picturesque/picturesque/settings.py
+++ picturesque/picturesque/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
