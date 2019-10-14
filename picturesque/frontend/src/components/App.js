823f401e35f3c406b9e15b647d99e5650089aeb8
Benjamin Choi <choi.bc97@gmail.com>
2019-10-14 06:41:00 +0000

    Initialize redux, accounts, and knox

--- picturesque/frontend/src/components/App.js
+++ picturesque/frontend/src/components/App.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 
 import Header from './layout/Header';
 
+import { Provider } from 'react-redux';
+import store from '../store';
+
 class App extends Component {
     render() {
-        return <Header />
+        <Provider store={store}>
+            return <Header />
+        </Provider>
     }
 }
 
--- picturesque/picturesque/settings.py
+++ picturesque/picturesque/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'frontend',
+    'knox',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSSES': ('knox.auth.TokenAuthentication')
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
