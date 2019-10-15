82d7f7f2d7d5eefde0b8610c97f994be85635e2d
Benjamin Choi <choi.bc97@gmail.com>
2019-10-15 00:21:14 +0000

    Implement alerts and messages

--- picturesque/frontend/src/components/App.js
+++ picturesque/frontend/src/components/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 
 import Header from './layout/Header';
 
 import { Provider } from 'react-redux';
 import store from '../store';
 
+// alert options
+const alertOptions = {
+    timeout: 3000,
+    position: 'top center',
+}
+
 class App extends Component {
     render() {
         <Provider store={store}>
-            return <Header />
+            <AlertProvider template={AlertTemplate} {...alertOptions}>
+                <Fragment>
+                    return <Header />
+                </Fragment>
+            </AlertProvider>
         </Provider>
     }
 }
