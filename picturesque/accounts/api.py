ce8ee816eaa0538e5c54c4d16f86a695fce048c4
Benjamin Choi <choi.bc97@gmail.com>
2019-10-29 20:56:42 +0000

    Fix issues with register

--- picturesque/accounts/api.py
+++ picturesque/accounts/api.py
@@ -12,10 +12,12 @@ class RegisterAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        _, token = AuthToken.objects.create(user)  # _ is instance
+
         return Response({
             'user': UserSerializer(
                 user, context=self.get_serializer_context()).data,
-            'token': AuthToken.objects.create(user)
+            'token': token
         })
 
 
@@ -27,8 +29,8 @@ class LoginAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
-
         _, token = AuthToken.objects.create(user)  # _ is instance
+
         return Response({
             'user': UserSerializer(
                 user, context=self.get_serializer_context()).data,
--- picturesque/frontend/src/actions/auth.js
+++ picturesque/frontend/src/actions/auth.js
@@ -79,7 +79,7 @@ export const register = ({ username, email, password }) => dispatch => {
   const body = JSON.stringify({ username, email, password });
 
   axios
-    .post("/api/auth/register/", body, config)
+    .post("/api/auth/register", body, config)
     .then(res => {
       dispatch({
         type: REGISTER_SUCCESS,
