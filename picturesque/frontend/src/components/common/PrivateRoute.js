d8ed55d14f822a0ebaaa598b3554f765b67ce273
Benjamin Choi <choi.bc97@gmail.com>
2019-11-19 20:55:27 +0000

    Remove unnecessary line

--- picturesque/frontend/src/components/common/PrivateRoute.js
+++ picturesque/frontend/src/components/common/PrivateRoute.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import PropTypes from "prop-types";
 
 const PrivateRoute = ({ component: Component, auth, ...rest }) => {
   return (
@@ -26,7 +25,4 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = {};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PrivateRoute);
+export default connect(mapStateToProps, mapDispatchToProps)(PrivateRoute);
